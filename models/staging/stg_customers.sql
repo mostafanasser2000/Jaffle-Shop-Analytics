e707cfdeccd68ddca7842609379a16398932519a
mostafa nasser <hobof76888@ahieh.com>
2024-05-14 16:29:59 +0000

    add staging models

---
+++ models/staging/stg_customers.sql
@@ -0,0 +1,14 @@
+with
+source as (
+    select * from {{ source('ecom', 'customers') }}
+),
+renamed as (
+    select
+        -- ids
+        id as customer_id,
+        -- text
+        name as customer_name
+    from source
+)
+
+select * from renamed
---
+++ models/staging/stg_order_items.sql
@@ -0,0 +1,16 @@
+with
+source as (
+    select * from {{ source('ecom', 'items') }}
+),
+
+renamed as (
+    select
+        -- ids
+        id as order_item_id,
+        order_id,
+        sku as product_id
+        
+    from source
+)
+
+select * from renamed
